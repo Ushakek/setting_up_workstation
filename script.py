83f7118933e1136a90a7e7935a90053e829d9c36
Ушаков Никита Сергеевич <Ushakov_NS>
2021-05-26 16:37:02 +0500

    Change download
    change download for windows

--- script.py
+++ script.py
@@ -17,51 +17,34 @@ class Script(object):
             self.system = 'win'
 
     def temp(self):
-        os.mkdir('temp')
+        try:
+            os.mkdir('temp')
+        except(FileExistsError):
+            print('Temp does already exist')
 
     def download_and_install_win(self):
-        url_python = 'https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe'
-        name_py = r'./temp/Python.exe'
-        url_pc = 'http://download.jetbrains.com/python/pycharm-community-2021.1.1.exe?_gl=1*1vc7rdn*_ga*Mjg3NTg4NjkzLjE2MjEzNDQ5MTc.*_ga_V0XZL7QHEB*MTYyMTg1OTAzNi4zLjEuMTYyMTg1OTI4OC4w&_ga=2.209779037.933493779.1621855103-287588693.1621344917'
-        name_pc = r'./temp/\PyCharm.exe'
-        url_sm = 'https://download.sublimetext.com/sublime_merge_build_2056_x64_setup.exe'
-        name_sm = r'./temp/\SublimeMerge.exe'
-        url_git = 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe'
-        name_git = r'./temp/\GIT.exe'
 
-        # download python3
-        py = open(name_py, 'wb')
-        download = requests.get(url_python)
-        py.write(download.content)
-        py.close()
+        urls = {'./temp/Python.exe': 'https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe',
+                './temp/PyCharm.exe': 'http://download.jetbrains.com/python/pycharm-community-2021.1.1.exe?_gl=1*1vc7rdn*_ga*Mjg3NTg4NjkzLjE2MjEzNDQ5MTc.*_ga_V0XZL7QHEB*MTYyMTg1OTAzNi4zLjEuMTYyMTg1OTI4OC4w&_ga=2.209779037.933493779.1621855103-287588693.1621344917',
+                './temp/SublimeMerge.exe': 'https://download.sublimetext.com/sublime_merge_build_2056_x64_setup.exe',
+                './temp/GIT.exe': 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe'}
 
-        # download pycharm
-        py_charm = open(name_pc, 'wb')
-        download = requests.get(url_pc)
-        py_charm.write(download.content)
-        py_charm.close()
-
-        # download sublime merge
-        sublime_merge = open(name_sm, 'wb')
-        download = requests.get(url_sm)
-        sublime_merge.write(download.content)
-        sublime_merge.close()
-
-        # download git
-        git = open(name_git, 'wb')
-        download = requests.get(url_git)
-        git.write(download.content)
-        git.close()
+        for prog in urls:
+            program = open(prog, 'wb')
+            download = requests.get(urls[prog])
+            program.write(download.content)
+            program.close()
 
         os.chdir('./temp')
         for file in range(0, 4):
-            os.system('start *.exe')
+            os.system('*.exe')
 
     def install_lin(self):
         os.system('sudo apt install python3')
         os.system('sudo apt install snap')
         os.system('sudo apt install git')
         os.system('sudo snap install pycharm-community --classic')
+        os.system('git clone https://gitlab-srv.corp.npkvip.ru/technological-processes/technological-process-smart-s-is')
 
 
 my_system = Script()
