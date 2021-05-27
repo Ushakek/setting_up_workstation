5c3ab752613c314ca38187cdcca845245fe40570
Ушаков Никита Сергеевич <Ushakov_NS>
2021-05-27 15:30:53 +0500

    Change install
    change install and download for windows

--- script.py
+++ script.py
@@ -23,7 +23,7 @@ class Script(object):
             print('Temp does already exist')
 
     def download_and_install_win(self):
-        programs_to_download = []
+        to_download = []
 
         urls = {'./temp/Python.exe': 'https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe',
                 './temp/PyCharm.exe': 'http://download.jetbrains.com/python/pycharm-community-2021.1.1.exe?_gl=1*1vc7rdn*_ga*Mjg3NTg4NjkzLjE2MjEzNDQ5MTc.*_ga_V0XZL7QHEB*MTYyMTg1OTAzNi4zLjEuMTYyMTg1OTI4OC4w&_ga=2.209779037.933493779.1621855103-287588693.1621344917',
@@ -31,25 +31,38 @@ class Script(object):
                 './temp/GIT.exe': 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe'}
 
         # проверим программы в папке temp
+        # programs = os.listdir('./temp')
+        # for program in urls:
+        #     if program in programs:
+        #         print('{} was downloaded'.format(program))
+        #     else:
+        #         programs_to_download.append(program)
+
+        # скачаем то, что ещё не скачано
+        # для каждого из пути
+        # for path in urls.keys():
+        #     # для каждого из НЕ скачанного
+        #     for i in to_download:
+        #         # если путь в "НЕ скачанном", то скачиваем
+        #         if path == i:
+        #             program = open(path, 'wb')
+        #             download = requests.get(urls[path])
+        #             program.write(download.content)
+        #             program.close()
+        # создаём список скачанных файлов
+        # В ПРОЦЕССЕ ПРИДУМЫВАНИЯ
+
+        # Скачиваем файлы
+        for path in urls.keys():
+            program = open(path, 'wb')
+            download = requests.get(urls[path])
+            program.write(download.content)
+            program.close()
+
         programs = os.listdir('./temp')
-        for program in urls:
-            if program in programs:
-                print('{} was downloaded'.format(program))
-            else:
-                programs_to_download.append(program)
-
-
-        for prog in urls:
-            for i in programs_to_download:
-                if prog == i:
-                    program = open(prog, 'wb')
-                    download = requests.get(urls[prog])
-                    program.write(download.content)
-                    program.close()
-
-        programs = os.listdir('./temp')
+        # перемешаемся в паку скачанных файлов
         os.chdir('./temp')
-
+        # запускаем установку
         for i in programs:
             os.system('{}'.format(i))
 
