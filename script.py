1da3f5d04698f3affdd1f40723c4f9b71beb9b2a
Ушаков Никита Сергеевич <Ushakov_NS>
2021-06-01 17:55:02 +0500

    Fix clone
    Исправил проблему клонирования репозитория с гитлаб для windows. Сейчас программа работает только при запуске из powershell

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 from sys import platform
 import requests
 import os
+import subprocess
 
 
 class Script(object):
     """
-    Класс с конструктором. Родитель остальных классов
+    Класс с конструктором. Родитель остальных классов. В нём находится определитель системы.
     """
     def __init__(self):
         # linux
@@ -22,11 +23,17 @@ class ForWidows(Script):
     Сценарий установки для Windows, взаимодействие осуществляется через командную строку
     """
     def download(self):
+        """
+        Сценарий для скачивания программ. Пути и названия программ записаны как ключи в словаре, а ссылки на
+        скачивание, как значения этих ключей. Запускаются циклом.
+        Перед началом цикла создаётся "Временная папка, куда загружаются устоновочные образы
+        """
 
         try:
             os.mkdir('temp')
-        except(FileExistsError):
+        except FileExistsError:  # ловим ошибку созданной папки
             print('Temp does already exist')
+            pass
 
         print('Start download!')
 
@@ -35,6 +42,10 @@ class ForWidows(Script):
                 './temp/SublimeMerge.exe': 'https://download.sublimetext.com/sublime_merge_build_2056_x64_setup.exe',
                 './temp/GIT.exe': 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe'}
 
+        # program = open('./temp/GIT.exe', 'wb')
+        # download = requests.get(urls['./temp/GIT.exe'])
+        # program.write(download.content)
+        # program.close()
         # Скачиваем файлы
         for path in urls.keys():
             program = open(path, 'wb')
@@ -42,11 +53,28 @@ class ForWidows(Script):
             program.write(download.content)
             program.close()
 
+    def reboot(self):
+        # перезагрузка командной строки методом запуска новой
+        print('Command of reboot cmd')
+        try:
+            subprocess.Popen('{}'.format(work_with_git()), creationflags=subprocess.DETACHED_PROCESS)
+        except FileNotFoundError:
+            pass
+
     def install(self):
+        """
+        Установка скачанных файлов.
+        Перед циклом создаёт список с программами, которые нужно будет установить, потом перемещется в расположение
+        скачанных файлов. Далее, используя цикл и автоподстановку запускается процесс установки программ, которые
+        регулирует "настройщик"
+        """
+
+        print('Start install!')
         # перемешаемся в паку скачанных файлов
         programs = os.listdir('./temp')
         # запускаем установку
         os.chdir('./temp')
+        # os.system('GIT.exe')
         for program in programs:
             os.system('{}'.format(program))
         os.chdir('../')
@@ -81,27 +109,39 @@ class ForLinux(Script):
     Сценарий установки для Linux, взаимодействие осуществляется через командную строку
     """
     def install(self):
+        """
+        Запускается установка через командную строку, требуется ввод пароля администратора
+        В начале запускается обновление списка пакетов, в конце запускается обновление списка пакетов, а следом
+        обновление этих пакетов.
+        """
+        os.system('sudo apt update')
         os.system('sudo apt install python3')
         os.system('sudo apt install snap')
         os.system('sudo apt install git')
         os.system('sudo snap install pycharm-community --classic')
+        os.system('sudo apt update')
+        os.system('sudo apt upgrade')
 
 
 def work_with_git():
-    # Функция общего сценария для обоих систем
+    # Функция общего сценария для обоих систем по "склониванию" репозитория
     print('yeah! we do this!')
     os.system('git config --global http.sslverify false')
     os.system('git clone https://gitlab-srv.corp.npkvip.ru/technological-processes/technological-process-smart-s-is')
 
 
 def process_of_install():
+    """
+    Эта функция регулирует процесс установки программ
+    """
     this_system = Script()
     print(this_system.system)
     if this_system.system == 'Your system is Windows':
         win = ForWidows()
         win.download()
         win.install()
-        work_with_git()
+        win.reboot()
+
     elif this_system.system == 'Your system is Linux':
         lin = ForLinux()
         lin.install()
@@ -109,4 +149,4 @@ def process_of_install():
 
 
 process_of_install()
-
+end = input('Installation finished\nPress enter to exit\n')
